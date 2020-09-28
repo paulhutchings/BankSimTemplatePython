724526635c9203ab885f99616996d758e659ee0e
paul <dev@studiop.page>
2020-09-28 15:24:27 -0400

    Switched to using ThreadPoolExecutor

--- src/banksim_main.py
+++ src/banksim_main.py
@@ -6,17 +6,24 @@
  @author Modified by Tarek Elseify
  @author Modified by Paul Hutchings
 """
-
-from transfer_thread import TransferThread
 from bank import Bank
+import concurrent.futures as cf, random, threading
 
-if __name__ == 'main':
+def transfer_thread(bank: Bank, from_account: int, max_amount: int):
+    for i in range(10000):
+        to_account = random.randint(0, bank.get_num_accounts() - 1)
+        amount = random.randint(0, max_amount)
+        bank.transfer(from_account, to_account, amount)
+    print(f'Thread {threading.get_ident()} has finished with its transactions.')
+
+if __name__ == "__main__":
     N_ACCOUNTS = 10
     INITIAL_BALANCE = 10000
-
     bank = Bank(N_ACCOUNTS, INITIAL_BALANCE)
-    threads = [TransferThread(bank, i, INITIAL_BALANCE) for i in range(N_ACCOUNTS)]
     print('Bank transfer is in progress...')
-    [thread.start() for thread in threads]
-    [thread.join() for thread in threads]
-    bank.test()
+    with cf.ThreadPoolExecutor(max_workers=N_ACCOUNTS) as executor:
+        futures = []
+        for i in range(N_ACCOUNTS):
+            futures.append(executor.submit(transfer_thread, (bank, i, INITIAL_BALANCE)))
+        cf.wait(futures)
+        bank.test()
